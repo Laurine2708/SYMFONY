96fb2a6fcae4ada731addeb0ce6cd797940c1cf2
Laurine2708 <lmoreau87@gmail.com>
2026-03-11 10:45:20 +0100

    Commit asset

---
+++ assets/vendor/installed.php
@@ -0,0 +1,101 @@
+<?php return array (
+  '@hotwired/stimulus' => 
+  array (
+    'version' => '3.2.2',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@hotwired/turbo' => 
+  array (
+    'version' => '7.3.0',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'chart.js' => 
+  array (
+    'version' => '3.9.1',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'stimulus' => 
+  array (
+    'version' => '3.2.2',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'vue' => 
+  array (
+    'version' => '3.5.22',
+    'dependencies' => 
+    array (
+      0 => '@vue/runtime-dom',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@vue/runtime-dom' => 
+  array (
+    'version' => '3.5.22',
+    'dependencies' => 
+    array (
+      0 => '@vue/runtime-core',
+      1 => '@vue/runtime-core',
+      2 => '@vue/shared',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@vue/runtime-core' => 
+  array (
+    'version' => '3.5.22',
+    'dependencies' => 
+    array (
+      0 => '@vue/reactivity',
+      1 => '@vue/reactivity',
+      2 => '@vue/shared',
+      3 => '@vue/shared',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@vue/shared' => 
+  array (
+    'version' => '3.5.22',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@vue/reactivity' => 
+  array (
+    'version' => '3.5.22',
+    'dependencies' => 
+    array (
+      0 => '@vue/shared',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+);
